741b6bb8b5e717c80e3930bc8b3305e77e871d1d
Denis <denis.alexandre200295@gmail.com>
2021-11-08 20:46:38 -0300

    arrumar

---
+++ src/Page/Home/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Seja bem vindo a o processo seletivo doGitHub</h1>
+
+        <a href="/Usuario">
+          <button className="btn">Entrar</button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
