47f5f70e9df5c6067947fe35d28ba290cf161610
unknown <48345049800>
2021-11-09 10:28:03 -0300

    finalizado

--- src/Page/Home/App.js
+++ src/Page/Home/App.js
@@ -1,19 +1,19 @@
-import './App.css';
+// import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Seja bem vindo a o processo seletivo doGitHub</h1>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Seja bem vindo a o processo seletivo doGitHub</h1>
 
-        <a href="/Usuario">
-          <button className="btn">Entrar</button>
-        </a>
-      </header>
-    </div>
-  );
-}
+//         <a href="/Usuario">
+//           <button className="btn">Entrar</button>
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 
-export default App;
+// export default App;
